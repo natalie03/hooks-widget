317dbcdb097b9c6f686b4c66a471c631befeab8b
Ginger Lee <gingerl@twitter.com>
2020-11-05 14:58:37 -0800

    Add use effect to update time display every second

--- src/components/Datetime.js
+++ src/components/Datetime.js
@@ -1,13 +1,35 @@
+import { useEffect, useState } from 'react';
+
 function Datetime() {
+  const [time, setTime] = useState({hour: '', minute: '', second: ''});
+  const [date, setDate] = useState('');
   let options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
-  const date = new Date().toLocaleDateString("en-US", options);
-  const [hour, minute] = new Date().toLocaleTimeString("en-US").split(/:| /);
+
+  const setTimeAndDate = () => {
+    setDate(new Date().toLocaleDateString("en-US", options));
+    const [h, m, s] = new Date().toLocaleTimeString("en-US").split(/:| /)
+    setTime({hour: h, minute: m, second: s});
+  };
+
+  useEffect(() => {
+    if (!date) {
+      setTimeAndDate();
+    }
+
+    const interval = setInterval(() => {
+      setTimeAndDate();
+    }, 1000);
+
+
+    return () => clearInterval(interval);
+  });
 
   // TODO: Update this when the time changes
+  const {hour, minute, second} = time;
   return (
     <div className="datetime">
       <p>{date}</p>
-      <p>{`${hour}:${minute}`}</p>
+      <p>{`${hour}:${minute}:${second}`}</p>
     </div>
   );
 }
